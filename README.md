4312d023fccb04fb14759094755d8f201258e323
Emily Su <emilyzqsu@gmail.com>
2024-09-21 14:43:34 -0400

    Finished install packages script and added initial files

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# toronto_crimes
+# Toronto Crimes (To Be Changed)
+
+## Overview
+
+
+
+
+## File Structure
+
+The repo is structured as:
+
+-   `data/raw_data` contains the raw data as obtained from the City of Toronto's Open Data Portal.
+-   `data/analysis_data` contains the cleaned dataset that was constructed.
+-   `other` contains sketches.
+-   `paper` contains the files used to generate the paper, including the Quarto document and reference bibliography file, as well as the PDF of the paper. 
+-   `scripts` contains the R scripts used to simulate, download and clean data.
+
+
+## Statement on LLM usage
+
+No LLMs were used for any aspect of this work.
---
+++ scripts/00-install_packages.R
@@ -0,0 +1,19 @@
+#### Preamble ####
+# Purpose: Installs packages needed to run scripts and Quarto document
+# Author: Emily Su
+# Date: 21 September 2024
+# Contact: em.su@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: -
+# NOTE: This script was checked through lintr for styling
+
+#### Workspace setup ####
+## Installing packages (only needs to be done once per computer)
+install.packages("tidyverse") # Contains data-related packages
+install.packages("opendatatoronto") # Open Data Toronto API
+install.packages("knitr") # To make tables
+install.packages("janitor") # To clean datasets
+install.packages("dplyr")
+install.packages("ggplot2") # To make graphs
+install.packages("lintr") # To check styling of code
+install.packages("styler") # To style code
