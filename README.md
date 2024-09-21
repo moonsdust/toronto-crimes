8800f5bc634351b93ce13b8ab2eb682b4044233c
Emily Su <emilyzqsu@gmail.com>
2024-09-21 15:46:20 -0400

    Finished download data script and added raw data

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 
 The repo is structured as:
 
--   `data/raw_data` contains the raw data as obtained from the City of Toronto's Open Data Portal.
+-   `data/raw_data` contains the raw data as obtained from the City of Toronto's Open Data Portal (Open Data Toronto).
 -   `data/analysis_data` contains the cleaned dataset that was constructed.
 -   `other` contains sketches.
 -   `paper` contains the files used to generate the paper, including the Quarto document and reference bibliography file, as well as the PDF of the paper. 
--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,52 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Open Data Toronto on the location
+# of police facilities, neighbourhood crime rates, and the profiles of
+# Toronto's 25 wards.
+# Author: Emily Su
+# Date: 21 September 2024
+# Contact: em.su@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: 00-install_packages.R has been ran to install necessary
+# packages.
+# Any other information needed? -
+# NOTE: This script was checked through lintr for styling
 
+# Datasets to be downloaded and saved
+# Ward Profiles (25-Ward Model):
+# - https://open.toronto.ca/dataset/ward-profiles-25-ward-model/
+# Police Facility Location
+# - https://open.toronto.ca/dataset/police-facility-locations/
+# Neighbourhood Crime Rates
+# - https://open.toronto.ca/dataset/neighbourhood-crime-rates/
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# Code to download data was adapted from:
+# https://open.toronto.ca/dataset/neighbourhood-crime-rates/
 
+# Function to download data
+download_csv <- function(package_num) {
+  files <- filter(list_package_resources(package_num),
+                  tolower(format) %in% c("csv"))
+  return(filter(files, row_number() == 1) %>% get_resource())
+}
 
+# Download "Neighbourhood Crime Rates" Dataset
+crime_rates <- download_csv("neighbourhood-crime-rates")
 
+# Download "Police Facility Locations" Dataset
+police_location <- download_csv("9aeefa17-27e8-4dd9-b74d-80f7f9eb85ac")
+
+# Download "Ward Profiles (25-Ward Model)"
+ward_profiles <-
+  filter(list_package_resources("6678e1a6-d25f-4dff-b2b7-aa8f042bc2eb"),
+         row_number() == 1) |> get_resource()
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+write_csv(crime_rates, "data/raw_data/raw_crime_rates.csv")
+write_csv(police_location, "data/raw_data/raw_police_location.csv")
+write_csv(as.data.frame(ward_profiles[1]),
+          "data/raw_data/raw_ward_profiles.csv")
