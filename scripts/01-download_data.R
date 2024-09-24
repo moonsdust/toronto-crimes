f0b937b277d77db81dee2bbab064a79bbb0b8f92
Emily Su <emilyzqsu@gmail.com>
2024-09-23 20:11:27 -0400

    Added new dataset for boundaries

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Downloads and saves the data from Open Data Toronto on the location
-# of police facilities, neighbourhood crime rates, and the profiles of
-# Toronto's 25 wards.
+# of police facilities, neighbourhood crime rates, the profiles of
+# Toronto's 25 wards, and neighbourhood boundaries.
 # Author: Emily Su
 # Date: 21 September 2024
 # Contact: em.su@mail.utoronto.ca
@@ -18,6 +18,8 @@
 # - https://open.toronto.ca/dataset/police-facility-locations/
 # Neighbourhood Crime Rates
 # - https://open.toronto.ca/dataset/neighbourhood-crime-rates/
+# Neighbourhoods
+# - https://open.toronto.ca/dataset/neighbourhoods/
 
 #### Workspace setup ####
 library(opendatatoronto)
@@ -45,8 +47,13 @@ police_location <- download_csv("9aeefa17-27e8-4dd9-b74d-80f7f9eb85ac")
 ward_profiles <-
   filter(list_package_resources("6678e1a6-d25f-4dff-b2b7-aa8f042bc2eb"),
          row_number() == 1) |> get_resource()
+
+# Download "Neighbourhood"
+neighbourhood_boundaries <- download_csv("neighbourhoods")
+
 #### Save data ####
 write_csv(crime_rates, "data/raw_data/raw_crime_rates.csv")
 write_csv(police_location, "data/raw_data/raw_police_location.csv")
 write_csv(as.data.frame(ward_profiles[1]),
           "data/raw_data/raw_ward_profiles.csv")
+write_csv(neighbourhood_boundaries, "data/raw_data/raw_neighbourhood.csv")
